f324e989a45dce10b3f9d61650cd64bdb0e44bf5
renzo <renvmorales@gmail.com>
2017-10-14 18:56:46 -0300

    Add comments on reading arrf files using scipy.io

--- supermarket.py
+++ supermarket.py
@@ -1,5 +1,3 @@
-
-
 from scipy.io import arff
 import numpy as np
 import pandas as pd
@@ -7,12 +5,14 @@ import pandas as pd
 
 
 
-
+# read supermarket arrf file available at 
+# http://storm.cis.fordham.edu/~gweiss/data-mining/datasets.html
+# .arrf files are Weka datasets types
 dataset = arff.loadarff('supermarket.arff')
-# print(dataset)
 
+
+# convert to dataframe using pandas
 df = pd.DataFrame(dataset[0])
-
 print(df.head(100))
 
 
