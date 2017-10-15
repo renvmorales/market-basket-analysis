00636e202585d96b762ef6e2d9db181711f2ec0e
renzo <renvmorales@gmail.com>
2017-10-14 21:56:57 -0300

    Check all dataframe entry types  Set apriori to min_support=0.3  Set a lift min_treshold=1.2 for the association rules

--- supermarket.py
+++ supermarket.py
@@ -7,6 +7,8 @@ from mlxtend.frequent_patterns import association_rules
 
 
 
+print('Loading raw supermarket basket data ...\n')
+
 # read supermarket arrf file available at 
 # http://storm.cis.fordham.edu/~gweiss/data-mining/datasets.html
 # .arrf files are Weka datasets types
@@ -16,6 +18,8 @@ dataset = arff.loadarff('supermarket.arff')
 
 # convert to dataframe using pandas
 df = pd.DataFrame(dataset[0])
+# print(df.head())
+
 
 
 # discard the last column ('total')
@@ -24,9 +28,16 @@ df = df.iloc[:,0:-1]
 
 
 
+# check entry types
+print('All possible entries in dataframe are:\n', 
+	pd.unique(df.values.ravel()))
 
-# create a encoder function for each transaction
-def encode_units(x):
+
+
+
+
+print('\nEncoding dataframe to binary values ...\n')
+def encode_units(x):  # encoder function for each transaction
     if str(x) == "b'?'":
         return 0
     if str(x) == "b't'":
@@ -34,15 +45,28 @@ def encode_units(x):
 
 
 
-
 # apply the encoder to the dataframe
 df_onehot = df.applymap(encode_units)
-print('Original Transaction table:\n')
-print(df_onehot.head())
+# print('Original Transaction table:\n', df_onehot.head())
 
 
 
 
 
 
+# find all fequent item sets with minimum support value
+#    OBS: support here is computed with respect to a single, or set 
+#    items. However, to find a support rule just assemble antescedant, and
+# 	 consequent items as a single item set.
+fq_itemsets = apriori(df_onehot, min_support=0.3, use_colnames=True)
+# print('\nFrequent item sets:\n', 
+# 	fq_itemsets.sort_values(by='support', ascending=False).head(100))
+
+
+
+
+# list confidence and lift metrics with respect to the association rules
+rules = association_rules(fq_itemsets, metric="lift", min_threshold=1.2)
+print('Most significant association rules are:\n',
+	rules.sort_values(by=['support','confidence'], ascending=False).head(10))
 
